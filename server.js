2d4c45613188ac576d0d4b718d9ac3f65b865327
ahsouza <annibalhsouza@gmail.com>
2019-02-08 18:25:40 -0200

    #ISSUE Preparing for deployment

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const
  path = require('path'),
  app = express()
 
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(serveStatic(__dirname + "/dist"))
 const port = process.env.PORT || 5000
 app.listen(port)
