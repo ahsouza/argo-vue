b3da57d843023e2ee1e7223f38d24a3c4d34521e
ahsouza <annibalhsouza@gmail.com>
2019-02-11 15:42:35 -0200

    #ISSUE Altering somethings

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const
  app = require('./express.config')(),
  port = process.env.PORT || 8080
-
-app.listen(port, () => { console.log(`Server listening in port ${port}`)})
+ app.listen(port, () => { console.log(`Server listening in port ${port}`)})
